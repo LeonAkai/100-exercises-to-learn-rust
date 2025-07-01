8eb3ab0bb5c740467159dad40c215b058063aabc
username <purplechameleon.app@gmail.com>
2025-07-01 21:39:04 +0900

    ドキュメンテーションコメントを日本語に翻訳し、panicメッセージのカスタム化を明示しました

--- exercises/02_basic_calculator/04_panics/src/lib.rs
+++ exercises/02_basic_calculator/04_panics/src/lib.rs
@@ -1,8 +1,10 @@
-/// Given the start and end points of a journey, and the time it took to complete the journey,
-/// calculate the average speed of the journey.
+/// 旅の出発点と到着点、それに要した時間を与えると、
+/// 旅の平均速度を計算して返します。
 fn speed(start: u32, end: u32, time_elapsed: u32) -> u32 {
-    // TODO: Panic with a custom message if `time_elapsed` is 0
-
+    // TODO: `time_elapsed` が 0 の場合はカスタムメッセージで panic させる
+    if time_elapsed == 0 {
+        panic!("The journey took no time at all. That's impossible!");
+    }
     (end - start) / time_elapsed
 }
 
@@ -16,9 +18,10 @@ mod tests {
     }
 
     #[test]
-    // 👇 With the `#[should_panic]` annotation we can assert that we expect the code
-    //    under test to panic. We can also check the panic message by using `expected`.
-    //    This is all part of Rust's built-in test framework!
+    // 👇 `#[should_panic]` アノテーションを付けると、テスト対象コードが
+    //    panic することを期待していると示せます。`expected` を使えば
+    //    panic メッセージの内容も検証できます。
+    //    これらは Rust 組み込みのテストフレームワークの機能です！
     #[should_panic(expected = "The journey took no time at all. That's impossible!")]
     fn by_zero() {
         speed(0, 10, 0);
